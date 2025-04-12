a9e89839495e0a42d02436dad692e44472d6ec8e
Emil Alvaro Serrano Danguilan <codexxxhost@gmail.com>
2025-04-12 14:26:16 +0800

    Update config.js

--- js/config/config.js
+++ js/config/config.js
@@ -16,7 +16,7 @@ export const CONFIG = {
     },
     AUDIO: {
         INPUT_SAMPLE_RATE: 16000,
-        OUTPUT_SAMPLE_RATE: 22000,
+        OUTPUT_SAMPLE_RATE: 26000,
         BUFFER_SIZE: 7680,
         CHANNELS: 1,
     },
